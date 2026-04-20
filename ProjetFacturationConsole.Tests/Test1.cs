88b70a5505089f05a97e51f724cbb25f30834d1a
AllanM <Allan.Mercier@edourdgand.fr>
2026-04-20 18:28:37 +0200

    Ajouts des tests Unitaires

--- ProjetFacturationConsole.Tests/Test1.cs
+++ ProjetFacturationConsole.Tests/Test1.cs
@@ -1,10 +1,39 @@
-﻿namespace ProjetFacturationConsole.Tests;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjetFacturationConsole.App;
+using System;
 
-[TestClass]
-public sealed class Test1
+namespace ProjetFacturationConsole.Tests
 {
-    [TestMethod]
-    public void TestMethod1()
+    [TestClass]
+    public class UnitTest1
     {
+        [TestMethod]
+        public void TestCalculerTotalHT()
+        {
+            LigneFacture ligne = new LigneFacture("Test", 2, 150, 20);
+            decimal totalHT = ligne.CalculerTotalHT();
+            Assert.AreEqual(300, totalHT);
+        }
+
+        [TestMethod]
+        public void TestCalculerTotalTTC()
+        {
+            LigneFacture ligne = new LigneFacture("Test", 2, 150, 20);
+            decimal totalTTC = ligne.CalculerTotalTTC();
+            Assert.AreEqual(360, totalTTC);
+        }
+
+        [TestMethod]
+        public void TestCalculerTotalTTCFacture()
+        {
+            Facture facture = new Facture("F2026-001", DateTime.Now, new Entreprise(1, "TechNova", "contact@technova.fr", "0322000001", "25 rue des Lilas", "Amiens", "80000", "12345678900011"),
+                new Client(1, "Paul Durand", "paul.durand@mail.fr", "0611223344", "12 rue Victor Hugo", "Amiens", "80000", DateTime.Now));
+
+            facture.AjouterLigne(new LigneFacture("Développement module connexion", 2, 150, 20));
+            facture.AjouterLigne(new LigneFacture("Maintenance corrective", 1, 80, 10));
+
+            decimal totalTTC = facture.CalculerTotalTTC();
+            Assert.AreEqual(448, totalTTC);
+        }
     }
 }
